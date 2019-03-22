2c7446c6991b9ff0ae465ecaee9e3714c3727ac1
Saso Matejina <>
2019-03-22 16:55:39 -0700

    Catch cursor errors in oplog

--- tap_mongodb/sync_strategies/oplog.py
+++ tap_mongodb/sync_strategies/oplog.py
@@ -3,6 +3,7 @@ from bson import objectid, timestamp
 import copy
 import pymongo
 import singer
+from bson import InvalidBSON
 from singer import metadata, metrics, utils
 
 import tap_mongodb.sync_strategies.common as common
@@ -88,53 +89,60 @@ def sync_oplog_stream(client, streams, state):
 
         with client.local.oplog.rs.find({'ts': {'$gt': oplog_ts}},
                                         oplog_replay=True) as cursor:
-            for row in cursor:
-                rows_saved += 1
+            
+            while cursor.alive:
+                try:
+                    row = next(cursor)
 
-                if row['op'] == 'n':
-                    LOGGER.debug('Skipping noop op')
-                elif not streams_map.get(generate_tap_stream_id_for_row(row)):
-                    ops_skipped = ops_skipped + 1
+                    if row['op'] == 'n':
+                        LOGGER.debug('Skipping noop op')
+                    elif not streams_map.get(generate_tap_stream_id_for_row(row)):
+                        ops_skipped = ops_skipped + 1
 
-                    if ops_skipped % UPDATE_BOOKMARK_PERIOD == 0:
-                        LOGGER.info("Skipped %s ops so far as they were not for selected tables; %s rows extracted",
-                                    ops_skipped,
-                                    rows_saved)
-                else:
-                    row_op = row['op']
-                    if row_op in ['i', 'u']:
-                        tap_stream_id = generate_tap_stream_id_for_row(row)
-                        stream_map_entry = streams_map[tap_stream_id]
-                        whitelisted_row = {k:v for k,v in row['o'].items() if k in stream_map_entry['columns']}
-                        record_message = common.row_to_singer_record(stream_map_entry['stream'],
-                                                                     whitelisted_row,
-                                                                     common.get_stream_version(tap_stream_id, state),
-                                                                     time_extracted)
-
-                        singer.write_message(record_message)
-
-                    elif row_op == 'd':
-                        tap_stream_id = generate_tap_stream_id_for_row(row)
-                        stream_map_entry = streams_map[tap_stream_id]
-
-                        # Delete ops only contain the _id of the row deleted
-                        whitelisted_row = {column_name:None for column_name in stream_map_entry['columns']}
-
-                        whitelisted_row['_id'] = row['o']['_id']
-                        whitelisted_row[SDC_DELETED_AT] = row['ts']
-
-                        record_message = common.row_to_singer_record(stream_map_entry['stream'],
-                                                                     whitelisted_row,
-                                                                     common.get_stream_version(tap_stream_id, state),
-                                                                     time_extracted)
-                        singer.write_message(record_message)
+                        if ops_skipped % UPDATE_BOOKMARK_PERIOD == 0:
+                            LOGGER.info("Skipped %s ops so far as they were not for selected tables; %s rows extracted",
+                                        ops_skipped,
+                                        rows_saved)
                     else:
-                        LOGGER.info("Skipping op for table %s as it is not an INSERT, UPDATE, or DELETE", row['ns'])
+                        rows_saved += 1
+                        row_op = row['op']
+                        if row_op in ['i', 'u']:
+                            tap_stream_id = generate_tap_stream_id_for_row(row)
+                            stream_map_entry = streams_map[tap_stream_id]
+                            whitelisted_row = {k:v for k,v in row['o'].items() if k in stream_map_entry['columns']}
+                            record_message = common.row_to_singer_record(stream_map_entry['stream'],
+                                                                        whitelisted_row,
+                                                                        common.get_stream_version(tap_stream_id, state),
+                                                                        time_extracted)
 
-                state = update_bookmarks(state,
-                                         streams_map,
-                                         row['ts'])
-                
+                            singer.write_message(record_message)
+
+                        elif row_op == 'd':
+                            rows_saved += 1
+                            tap_stream_id = generate_tap_stream_id_for_row(row)
+                            stream_map_entry = streams_map[tap_stream_id]
+
+                            # Delete ops only contain the _id of the row deleted
+                            whitelisted_row = {column_name:None for column_name in stream_map_entry['columns']}
+
+                            whitelisted_row['_id'] = row['o']['_id']
+                            whitelisted_row[SDC_DELETED_AT] = row['ts']
+
+                            record_message = common.row_to_singer_record(stream_map_entry['stream'],
+                                                                        whitelisted_row,
+                                                                        common.get_stream_version(tap_stream_id, state),
+                                                                        time_extracted)
+                            singer.write_message(record_message)
+                        else:
+                            LOGGER.info("Skipping op for table %s as it is not an INSERT, UPDATE, or DELETE", row['ns'])
+
+                    state = update_bookmarks(state,
+                                             streams_map,
+                                             row['ts'])
+                except InvalidBSON as e:
+                    LOGGER.info(e)
+                    continue
+
                 if rows_saved % 1000 == 0:
                     singer.write_state(state)
 
